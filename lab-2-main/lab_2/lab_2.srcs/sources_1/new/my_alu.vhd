6b096b47f7fc4fc30826d40e9d46986338d6ee5f
dd1038 <157773849+dd1038@users.noreply.github.com>
2024-02-29 14:49:45 -0500

    Add files via upload

---
+++ lab-2-main/lab_2/lab_2.srcs/sources_1/new/my_alu.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/29/2024 02:00:40 PM
+-- Design Name: 
+-- Module Name: my_alu - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity my_alu is
+Port (A, B: in std_logic_vector (3 downto 0);
+Output: out std_logic_vector (3 downto 0);
+
+);
+end my_alu;
+
+architecture Behavioral of my_alu is
+
+
+begin
+Process(alu) is
+begin
+
+case alu is
+when "0000" =>  --0
+
+when "0001" =>  --1
+
+when "0010" =>  --2
+
+when "0011" =>  --3
+
+when "0100" =>  --4
+
+when "0101" =>  --5
+
+when "0110" =>  --6
+
+when "0111" =>  --7
+
+when "1000" =>  --8
+
+when "1001" =>  --9
+Output <= not(A);
+when "1010" =>  --A
+Output <= (A and B);
+when "1011" =>  --B
+Output <= (A or B);
+when "1100" =>  --C
+Output <= (A xor B);
+when "1101" =>  --D
+Output <= (A xnor B);
+when "1110" =>  --E
+Output <= (A nand B);
+when "1111" =>  --F
+Output <= (A nor B);
+
+end case;
+
+end Behavioral;
---
+++ lab-2-main/lab_2/lab_2.srcs/sources_1/new/ripple_adder.vhd
@@ -0,0 +1,64 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/22/2024 11:36:19 PM
+-- Design Name: 
+-- Module Name: ripple_adder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ripple_adder is
+Port (A,B: in std_logic_vector(3 downto 0);
+Cin: in std_logic;
+S: out std_logic_vector(3 downto 0);
+Cout: out std_logic
+);
+end ripple_adder;
+
+architecture ripple_adder_structural of ripple_adder is
+signal Cout1, Cout2, Cout3, Cout4: std_logic;
+signal S0, S1, S2, S3: std_logic;
+begin
+
+S0 <= A(0) xor B(0) xor Cin;
+Cout1 <= (A(0) and B(0)) xor (A(0) and Cin) xor (B(0) and Cin);
+
+S1 <= A(1) xor B(1) xor Cout1;
+Cout2 <= (A(1) and B(1)) xor (A(1) and Cout1) xor (B(1) and Cout1);
+
+
+S2 <= A(2) xor B(2) xor Cout2;
+Cout3 <= (A(2) and B(2)) xor (A(2) and Cout2) xor (B(2) and Cout2);
+
+S3 <= A(3) xor B(3) xor Cout3;
+Cout4 <= (A(3) and B(3)) xor (A(3) and Cout3) xor (B(3) and Cout3);
+
+
+Cout <= Cout4;
+S <= (S0&S1&S2&S3);
+
+end ripple_adder_structural;
